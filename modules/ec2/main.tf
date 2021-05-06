13a42c7694d6fcd3a1b11ffe2a9897df5e933552
Sanoob Thekke Valappil <SanoobTV@tca.gov.au>
2021-05-06 13:00:10 +1000

    Updated MainFile and Modules

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,11 +1,21 @@
-data "aws_ssm_parameter" "ami" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
 }
 
 
-
 resource "aws_instance" "webserver" {
-  ami = data.aws_ssm_parameter.ami.id
+  ami = data.aws_ami.latest-ubuntu.id
   instance_type = "t1.micro"
   associate_public_ip_address = true
   vpc_security_group_ids = var.sgid
