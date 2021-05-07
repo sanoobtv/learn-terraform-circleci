68e2cc7a45e1de1107499b728b1e2d513ebfc12b
Sanoob Thekke Valappil <SanoobTV@tca.gov.au>
2021-05-07 12:47:17 +1000

    Migrated SG to Variables

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "vpc" {
 
 module "ec2" {
   source = "./modules/ec2"
-  sgid= [module.vpc.publicSGid]
+  sgid= module.vpc.publicSGid
   subnetid=module.vpc.subnet_id
   #region= var.main_region
 }
