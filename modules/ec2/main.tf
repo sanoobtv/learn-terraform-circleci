c087c4cf3950f6b77e236489f214fcef6fd61ab0
Sanoob Thekke Valappil <SanoobTV@tca.gov.au>
2021-05-14 13:28:48 +1000

    Added KeyPair

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -29,4 +29,8 @@ resource "aws_instance" "webserver" {
   subnet_id = var.subnetid
   security_groups = [var.sgid]
   key_name = aws_key_pair.SSHkeyforWebserver.key_name
+  provisioner "file" {
+  source      = "script.sh"
+  destination = "/tmp/script.sh"
+  }
 }
