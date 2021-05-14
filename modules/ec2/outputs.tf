ba7b2d6cc1185e02c929e1fae37662578271541c
Sanoob Thekke Valappil <SanoobTV@tca.gov.au>
2021-05-14 11:59:13 +1000

    Added KeyPair

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -2,10 +2,7 @@ output "publicip" {
   value = aws_instance.webserver.public_ip
 }
 
-output "sshKey" {
-  value = aws_key_pair.SSHkeyforWebserver.
-  sensitive = false
-}
+
 
 output "sshKeyname" {
   value = aws_key_pair.SSHkeyforWebserver.key_name
